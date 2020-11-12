527252272305cd330b2cf3859a66d88b1c331194
juliefie1 <fie.julie@gmail.com>
2020-11-12 22:42:24 +0100

    ajout de la classe SimpleTest.java

---
+++ SimpleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SimpleTest {
+	@Test
+	public void test1() {
+		int a = 2;
+		int b = 2;
+		assertTrue(a+b==4);
+	}
+}
